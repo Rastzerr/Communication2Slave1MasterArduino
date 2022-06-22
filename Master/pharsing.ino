1a91493dab2f851b22f63ddbcc7e4aa2d0c390a7
Rastzerr <106707336+Rastzerr@users.noreply.github.com>
2022-06-23 01:30:48 +0700

    memperbaiki protokol

--- Master/pharsing.ino
+++ Master/pharsing.ino
@@ -1,44 +1,48 @@
-void pharsing(){
-  if(comComp1 == true){ //vt&{nilai pembagi tegangan}$acs&{nilai arus}$
-    for (int i = 0; i < inString1.length(); i++){
-      if(inString1[i] == '&'){
+void pharsing() {
+  if (comComp1 == true) { //$vt&{nilai pembagi tegangan}$acs&{nilai arus}
+    for (int i = 0; i < inString1.length(); i++) {
+      if (inString1[i] == '&') {
         lastString1 = strData1;
         strData1 = "";
       }
-      else if(inString1[i] == '$'){
+      else if (inString1[i] == '$') {
         lastString1 = "";
       }
-      else{
-        if(lastString1 == ""){
-         strData1 += inString1[i]; 
+      else {
+        if (lastString1 == "") {
+          strData1 += inString1[i];
         }
-        if(lastString1 == "vt"){
-        pembagiTegangan += inString1[i]; 
+        if (lastString1 == "vt") {
+          pembagiTegangan += inString1[i];
         }
-        if(lastString1 == "acs"){
+        if (lastString1 == "acs") {
           arus += inString1[i];
         }
       }
+      phars1 = true;
+      comComp1 = false;
     }
   }
 
-  if(comComp2 == true){ //frek&{nilai frekuensi}$
-    for (int j = 0; j < inString2.length(); j++){
-      if(inString2[j] == '&'){
+  if (comComp2 == true) { //$frek&{nilai frekuensi}
+    for (int j = 0; j < inString2.length(); j++) {
+      if (inString2[j] == '&') {
         lastString2 = strData2;
         strData2 = "";
       }
-      else if (inString2[j] == '$'){
+      else if (inString2[j] == '$') {
         lastString2 = "";
       }
       else {
-        if(lastString2 == ""){
+        if (lastString2 == "") {
           strData2 += inString2[j];
         }
-        if(lastString2 == "frek"){
+        if (lastString2 == "frek") {
           frek += inString2[j];
         }
       }
     }
+    phars2 = true;
+    comComp2 = false;
   }
 }
