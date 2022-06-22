ca7fc5306ba3203555a225f7043584791906f06b
Rastzerr <106707336+Rastzerr@users.noreply.github.com>
2022-06-23 01:32:08 +0700

    mengubah saluran komunikasi serial

--- Master/serialEvent.ino
+++ Master/serialEvent.ino
@@ -1,6 +1,6 @@
 void serialEvent() {  //read slave 1
-  while (Serial.available()){
-    char inChar = (char)Serial.read();
+  while (Serial1.available()){
+    char inChar = (char)Serial1.read();
     inString1 += inChar;
     if(inChar == '\n'){
       comComp1 = true;
@@ -8,9 +8,9 @@ void serialEvent() {  //read slave 1
   }
 }
 
-void serialEvent1() { //read slave 2
-  while (Serial1.available()){
-    char inChar = (char)Serial1.read();
+void serialEvent() { //read slave 2
+  while (Serial2.available()){
+    char inChar = (char)Serial2.read();
     inString2 += inChar;
     if(inChar == '\n'){
       comComp2 = true;
